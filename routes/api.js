ea158a21f4705ba0ab9824bac439d19412522f58
sathishkannan162 <sathishkannan162@gmail.com>
2022-11-05 16:04:29 +0530

    feat: handle route /api/translate

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,59 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
+
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      if (req.body.hasOwnProperty('locale') && req.body.hasOwnProperty('text')) {
+        if (req.body.text == '') {
+          res.json({
+            error: 'No text to translate'
+          })
+        }
+        else {
+          if (req.body.locale == 'american-to-british') {
+            let translation = translator.translateToBritish(req.body.text);
+            if (translation == req.body.text) {
+              res.json({
+                translation: 'Everything looks good to me!',
+                submission: req.body.text
+              })
+            }
+            else {
+              res.json({
+                translation: translation,
+                submission: req.body.text
+              })
+            }
+          }
+          else if (req.body.locale == 'british-to-american') {
+            let translation = translator.translateToAmerican(req.body.text);
+            if (translation == req.body.text) {
+              res.json({
+                translation: 'Everything looks good to me!',
+                submission: req.body.text
+              })
+            }
+            else {
+              res.json({
+                translation: translation,
+                submission: req.body.text
+              })
+            }
+          }
+          else {
+            res.json({
+              error: 'Invalid value for locale field'
+            })
+          }
+        }
+      }
+      else {
+        res.json({
+          error: 'Required field(s) missing'
+        })
+      }
     });
 };
