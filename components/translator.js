f9537e34e6d3a86ed1f0770560df6c50be1e5bb2
sathishkannan162 <sathishkannan162@gmail.com>
2022-11-05 13:51:24 +0530

    feat: (fix/feat) fix mistakes in translators and translated words are highlighted
    fix: mistakes in translate to american and british
    
    feat: translator gives translated words highlighted

--- components/translator.js
+++ components/translator.js
@@ -9,46 +9,63 @@ class Translator {
     }
 
     translateToBritish(sentence) {
-        let words = this.parserInput(sentence);
-        for (let i = 0; i < words.length; i++) {
-            let rem = '';
-            if (!/^\w+$/.test(words[i])) {
-                let newWord = words[i].slice(0, -1);
-                rem = words[i].slice(-1);
-                words[i] = newWord;
-            }
-            if (americanToBritishSpelling.hasOwnProperty(words[i])) {
-                words[i] = americanToBritishSpelling[words[i]];
-            } else if (americanOnly.hasOwnProperty(words[i])) {
-                words[i] = americanOnly[words[i]];
-            }
-            words[i] += rem;
+        let americanSpellKeys = Object.keys(americanToBritishSpelling);
+        let americanSpellVals = Object.values(americanToBritishSpelling);
+        let americanOnlyKeys = Object.keys(americanOnly);
+        let americanOnlyVals = Object.values(americanOnly);
+        let americanTitleKeys = Object.keys(americanToBritishTitles);
+        let americanTitleVals = Object.values(americanToBritishTitles);
+        let newSentence = sentence.concat('');
+
+        for (let i=0; i<americanSpellKeys.length; i++) {
+            let match = '(?<!-)'+americanSpellKeys[i]+'([\\W])';
+            let regex = new RegExp(match,'gi');
+            newSentence = newSentence.replace(regex,'<span class="highlight">'+americanSpellVals[i]+'</span>'+'$1');
+            
         }
-        return words.join(' ');
+        for (let i=0; i<americanOnlyKeys.length; i++) {
+            let match = '(?<!-)'+americanOnlyKeys[i]+'([\\W])';
+            let regex = new RegExp(match,'gi');
+            newSentence = newSentence.replace(regex,'<span class="highlight">'+americanOnlyVals[i]+'</span>'+'$1');
+        }
+        for (let i=0; i<americanTitleKeys.length; i++) {
+            
+            let match = americanTitleKeys[i]+'([\\W])';
+            let regex = new RegExp(match,'gi');
+            americanTitleVals[i] = americanTitleVals[i][0].toUpperCase() + americanTitleVals[i].slice(1);
+            newSentence = newSentence.replace(regex,'<span class="highlight">'+americanTitleVals[i]+'</span>'+'$1');
+        }
+        newSentence = newSentence.replace(/(\d+)\:(\d+)/g,'<span class="highlight">$1.$2</span>')
+        return newSentence;
     }
     translateToAmerican(sentence) {
-        let words = this.parserInput(sentence);
-        let britishWords = Object.values(americanToBritishSpelling);
-        let americanWords = Object.keys(americanToBritishSpelling);
-        let index;
-        let rem;
-        let newWord;
-        for (let i = 0; i < words.length; i++) {
-            rem = '';
-            if (!/^\w+$/.test(words[i])) {
-                newWord = words[i].slice(0, -1);
-                rem = words[i].slice(-1);
-                words[i] = newWord;
-            }
-            index = britishWords.indexOf(words[i]);
-            if (  index !== -1) {
-                words[i] = americanWords[index];
-            } else if (britishOnly.hasOwnProperty(words[i])) {
-                words[i] = britishOnly[words[i]];
-            }
-            words[i] += rem;
+        let americanSpellKeys = Object.keys(americanToBritishSpelling);
+        let americanSpellVals = Object.values(americanToBritishSpelling);
+        let britishOnlyKeys = Object.keys(britishOnly);
+        let britishOnlyVals = Object.values(britishOnly);
+        let americanTitleKeys = Object.keys(americanToBritishTitles);
+        let americanTitleVals = Object.values(americanToBritishTitles);
+        let newSentence = sentence.concat('');
+        for (let i=0; i<americanSpellVals.length; i++) {
+            let match = '(?<!-)'+americanSpellVals[i]+'([\\W])';
+            let regex = new RegExp(match,'gi');
+            newSentence = newSentence.replace(regex,'<span class="highlight">'+americanSpellKeys[i]+'</span>'+'$1');
+            
         }
-        return words.join(' ');
+        for (let i=0; i<britishOnlyKeys.length; i++) {
+            let match = '(?<!-)'+britishOnlyKeys[i]+'([\\W])';
+            let regex = new RegExp(match,'gi');
+            newSentence = newSentence.replace(regex,'<span class="highlight">'+britishOnlyVals[i]+'</span>'+'$1');
+        }
+        for (let i=0; i<americanTitleVals.length; i++) {
+            
+            let match = americanTitleVals[i]+'([\\W])';
+            let regex = new RegExp(match,'gi');
+            americanTitleKeys[i] = americanTitleKeys[i][0].toUpperCase() + americanTitleKeys[i].slice(1);
+            newSentence = newSentence.replace(regex,'<span class="highlight">'+americanTitleKeys[i]+'</span>'+'$1');
+        }
+        newSentence = newSentence.replace(/(\d+)\.(\d+)/g,'<span class="highlight">$1:$2</span>')
+        return newSentence;
     }
 }
 
